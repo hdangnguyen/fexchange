63a5729d4c49b98afb5f1a7d752e2013bc3d47a5
hdangnguyen <haidangprod@gmail.com>
2022-07-11 12:39:23 +0700

    Test token

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,8 +4,7 @@ export function UserIsValid(token) {
   console.log("the token is " + token.user);
 
   var decodedToken = jwt.decode(token);
-  console.log("decoded token is " + decodedToken.payload);
-  console.log("token user " + token.user);
+  console.log(decodedToken)
   var dateNow = new Date();
   if (decodedToken.exp > dateNow.getTime() / 1000) return true;
   else return false;
