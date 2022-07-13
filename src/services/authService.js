ece36b7b191c455d720d4a5301224fd01a50a4c5
hdangnguyen <hdangprod@gmail.com>
2022-07-13 11:58:17 +0700

    update login

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,24 +1,26 @@
 import jwt from "jsonwebtoken";
-import axiosClient from './../utils/api/axiosClient';
-import axios  from 'axios';
+import axiosClient from "./../utils/api/axiosClient";
+import axios from "axios";
 
 export async function UserIsValid(token) {
   let bodyFormData = new FormData();
-  bodyFormData.append('tokenId', token);
+  bodyFormData.append("tokenId", token);
   const responseToken = await axios({
     method: "POST",
-    url: process.env.REACT_APP_API_URL + '/api/login/google',
+    url: process.env.REACT_APP_API_URL + "/api/login/google",
     headers: {
-      "Content-Type" : "multipart/form-data",
+      "Content-Type": "multipart/form-data",
     },
-    data: bodyFormData  
-  }).then((res) => {
-    return res.data.token
-    // console.log(res.data)  
-  }).catch((err) => {
-    console.log(err)
+    data: bodyFormData,
   })
-  
+    .then((res) => {
+      return res.data.token;
+      // console.log(res.data)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   var decodedToken = jwt.decode(responseToken);
   var dateNow = new Date();
   if (decodedToken.exp > dateNow.getTime() / 1000) return responseToken;
