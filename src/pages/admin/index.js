b58f1024a1055ee193728dfafe045f17daf6e116
VietNNSE161326 <105256611+VietNNSE161326@users.noreply.github.com>
2022-07-24 14:00:00 +0700

    add sidebar

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,79 +1,51 @@
 import PropTypes from "prop-types";
 import React, { Fragment, useState, useEffect } from "react";
 import MetaTags from "react-meta-tags";
-import Paginator from "react-hooks-paginator";
-import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import { connect } from "react-redux";
-import { getSortedProducts } from "../../helpers/product";
-import LayoutOne from "../../layouts/LayoutOne";
-import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
-import ShopSidebar from "../../wrappers/product/ShopSidebar";
-import ShopTopbar from "../../wrappers/product/ShopTopbar";
-import ShopProducts from "../../wrappers/product/ShopProducts";
+import "./admin.css";
 
-const Admin = ({ location, products }) => {
-  const [layout, setLayout] = useState("grid three-column");
-  const [sortType, setSortType] = useState("");
-  const [sortValue, setSortValue] = useState("");
-  const [filterSortType, setFilterSortType] = useState("");
-  const [filterSortValue, setFilterSortValue] = useState("");
-  const [offset, setOffset] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [currentData, setCurrentData] = useState([]);
-  const [sortedProducts, setSortedProducts] = useState([]);
-
-  const pageLimit = 15;
-  const { pathname } = location;
-
-  const getLayout = (layout) => {
-    setLayout(layout);
-  };
-
-  const getSortParams = (sortType, sortValue) => {
-    setSortType(sortType);
-    setSortValue(sortValue);
-  };
-
-  const getFilterSortParams = (sortType, sortValue) => {
-    setFilterSortType(sortType);
-    setFilterSortValue(sortValue);
-  };
-
-  useEffect(() => {
-    let sortedProducts = getSortedProducts(products, sortType, sortValue);
-    const filterSortedProducts = getSortedProducts(
-      sortedProducts,
-      filterSortType,
-      filterSortValue
-    );
-    sortedProducts = filterSortedProducts;
-    setSortedProducts(sortedProducts);
-    setCurrentData(sortedProducts.slice(offset, offset + pageLimit));
-  }, [offset, products, sortType, sortValue, filterSortType, filterSortValue]);
-
+const Admin = () => {
   return (
     <Fragment>
       <MetaTags>
         <title>Flone | Admin</title>
         <meta name="description" content="Page for admin" />
       </MetaTags>
-      <LayoutOne>
-        <div className="shop-area pt-95 pb-100">
-          <div className="container-fluid">
-            <div className="row">
+      <Fragment>
+        <header>
+          <nav
+            id="sidebarMenu"
+            class="collapse d-lg-block sidebar collapse bg-white"
+          >
+            <div class="position-sticky">
+
+              {/* put a logo here */}
               
+              <div class="list-group list-group-flush mx-3">
+                <div></div>
+                <a
+                  href="/"
+                  class="list-group-item list-group-item-action py-2 ripple active"
+                >
+                  <span>User Management</span>
+                </a>
+                <a
+                  href="/"
+                  class="list-group-item list-group-item-action py-2 ripple"
+                >
+                  <span>Product Management</span>
+                </a>
+                <div></div>
+              </div>
             </div>
-          </div>
-        </div>
-      </LayoutOne>
+          </nav>
+        </header>
+      </Fragment>
     </Fragment>
   );
 };
 
-Admin.propTypes = {
-  location: PropTypes.object,
-  products: PropTypes.array,
-};
+Admin.propTypes = {};
 
 const mapStateToProps = (state) => {
   return {
